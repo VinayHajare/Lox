e1f16ce60d6f05ef05c0aee5ee2fa4f1701d0b2e
Vinay Arjun Hajare <vinayhajare2004@gmail.com>
2025-12-27 21:52:41 +0530

    Added the test-suit and fix small bugs in JLox. Updated the rust test-runner script.

--- test-runner/target/release/deps/test_runner.d
+++
@@ -1,5 +0,0 @@
-D:\Vinay Hajare\Lanuguage Hacking\Lox\test-runner\target\release\deps\test_runner.d: src\main.rs
-
-D:\Vinay Hajare\Lanuguage Hacking\Lox\test-runner\target\release\deps\test_runner.exe: src\main.rs
-
-src\main.rs:
--- test-runner/target/release/test-runner.d
+++
@@ -1,1 +0,0 @@
-D:\Vinay\ Hajare\Lanuguage\ Hacking\Lox\test-runner\target\release\test-runner.exe: D:\Vinay\ Hajare\Lanuguage\ Hacking\Lox\test-runner\src\main.rs
