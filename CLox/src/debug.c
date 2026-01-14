fa69ad8f163aa06c53996364cda3e47fbdcecb7f
Vinay Arjun Hajare <vinayhajare2004@gmail.com>
2026-01-14 19:58:48 +0530

    Implement method support: add OP_INVOKE and OP_METHOD opcodes, enhance class and instance handling, and update related compiler and VM logic.

--- CLox/src/debug.c
+++ CLox/src/debug.c
@@ -44,6 +44,16 @@ static int constantInstruction(const char *name, Chunk *chunk, int offset)
     return offset + 2;
 }
 
+static int invokeInstruction(const char *name, Chunk *chunk, int offset)
+{
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
+}
+
 static int longConstantInstruction(const char *name, Chunk *chunk, int offset)
 {
     uint32_t constant = chunk->code[offset + 1] |
@@ -205,8 +215,12 @@ int disassembleInstruction(Chunk *chunk, int offset)
         return simpleInstruction("OP_RETURN", offset);
     case OP_CLASS:
         return constantInstruction("OP_CLASS", chunk, offset);
+    case OP_INVOKE:
+        return invokeInstruction("OP_INVOKE", chunk, offset);
     case OP_CLASS_LONG:
         return longConstantInstruction("OP_CLASS_LONG", chunk, offset);
+    case OP_METHOD:
+        return constantInstruction("OP_METHOD", chunk, offset);
     default:
         printf("Unknown opcode %d\n", instruction);
         return offset + 1;
